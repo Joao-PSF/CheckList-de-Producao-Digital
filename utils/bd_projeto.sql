bee787d3a1e59ef77a9589c382367474f6cf6ad3
Joao-PSF <59989725+Joao-PSF@users.noreply.github.com>
2025-10-01 21:39:59 -0300

    Revise SQL schema and insert data for various tables
    Updated SQL script with new data inserts and modified table structures, including changes to columns and auto-increment values.

--- utils/bd_projeto.sql
+++ utils/bd_projeto.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 01/09/2025 às 03:35
+-- Tempo de geração: 02/10/2025 às 02:39
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.2.12
 
@@ -32,6 +32,15 @@ CREATE TABLE `acessosniveis` (
   `nivel` tinyint(4) NOT NULL,
   `descricao` varchar(150) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `acessosniveis`
+--
+
+INSERT INTO `acessosniveis` (`id`, `nivel`, `descricao`) VALUES
+(1, 1, 'Admin'),
+(2, 2, 'Operador'),
+(3, 3, 'Supervisor');
 
 -- --------------------------------------------------------
 
@@ -116,11 +125,27 @@ CREATE TABLE `servicos_os` (
   `servico_tipo_id` int(11) NOT NULL COMMENT 'Tipo de Serviço',
   `nome_cliente` varchar(250) DEFAULT NULL COMMENT 'Nome do Cliente',
   `numero_cliente` varchar(250) DEFAULT NULL COMMENT 'Contato do Cliente',
-  `criado_por` int(11) NOT NULL COMMENT 'Matricula do Gerador da OS',
+  `criado_por_cpf` int(11) NOT NULL COMMENT 'Matricula do Gerador da OS',
   `criado_em` date NOT NULL,
+  `data_inicio` date DEFAULT NULL,
   `data_programada` date DEFAULT NULL COMMENT 'Data Programada para Encerramento',
-  `status` varchar(7) NOT NULL
+  `data_encerramento` date DEFAULT NULL,
+  `situacao` varchar(20) NOT NULL DEFAULT 'ANDAMENTO' COMMENT 'ANDAMENTO/PENDENTE/ENCERRADA',
+  `status` varchar(20) NOT NULL COMMENT 'Ativo/Inativo?'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='Tabela de Serviços/Tarefas';
+
+--
+-- Despejando dados para a tabela `servicos_os`
+--
+
+INSERT INTO `servicos_os` (`id`, `servico_tipo_id`, `nome_cliente`, `numero_cliente`, `criado_por_cpf`, `criado_em`, `data_inicio`, `data_programada`, `data_encerramento`, `situacao`, `status`) VALUES
+(7, 1, 'dasdas', 'dasd', 222222222, '2025-09-26', NULL, '2025-09-27', NULL, 'PENDENTE', 'Ativo'),
+(8, 2, 'jhgjhgj', 'ghjgh', 222222222, '2025-09-26', NULL, '2025-09-26', NULL, 'PENDENTE', 'Ativo'),
+(9, 2, 'jhgjhgj', 'ghjgh', 222222222, '2025-09-26', NULL, '2025-09-26', NULL, 'PENDENTE', 'Ativo'),
+(10, 1, 'bnbvn', 'vbnvbnv', 222222222, '2025-09-26', NULL, '2025-09-26', NULL, 'PENDENTE', 'Ativo'),
+(11, 1, 'fgfcv', 'cxvcx', 222222222, '2025-09-26', NULL, '2025-09-26', NULL, 'PENDENTE', 'Ativo'),
+(12, 1, 'sdsad', 'asdasd', 222222222, '2025-09-26', NULL, '2025-09-26', NULL, 'PENDENTE', 'Ativo'),
+(13, 1, 'teste1', 'teste1', 2147483647, '2025-09-28', NULL, '2025-09-28', NULL, 'PENDENTE', 'Ativo');
 
 -- --------------------------------------------------------
 
@@ -133,6 +158,14 @@ CREATE TABLE `servicos_tipos` (
   `tipo` varchar(250) NOT NULL COMMENT 'Tipo de Serviço',
   `status` varchar(7) NOT NULL COMMENT 'Ativo/Inativo?'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='Tipos de Serviços';
+
+--
+-- Despejando dados para a tabela `servicos_tipos`
+--
+
+INSERT INTO `servicos_tipos` (`id`, `tipo`, `status`) VALUES
+(1, 'Manutenção', 'Ativo'),
+(2, 'Outro', 'Ativo');
 
 -- --------------------------------------------------------
 
@@ -150,6 +183,15 @@ CREATE TABLE `servico_etapas` (
   `servico_os_id` int(11) NOT NULL COMMENT 'ID da Ordem de Serviço',
   `status` varchar(7) NOT NULL COMMENT 'Ativo/Inativo?'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `servico_etapas`
+--
+
+INSERT INTO `servico_etapas` (`id`, `etapa`, `ordem`, `execucao`, `criada_em`, `executada_em`, `servico_os_id`, `status`) VALUES
+(1, 'Vistoria Inicial', 1, 0, '2025-09-18', NULL, 1, 'Ativo'),
+(2, 'Execução do Serviço', 1, 0, '2025-09-18', NULL, 2, 'Ativo'),
+(3, 'Entrega e Fechamento', 1, 1, '2025-09-13', '2025-09-15', 3, 'Ativo');
 
 -- --------------------------------------------------------
 
@@ -201,12 +243,23 @@ CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `matricula` int(11) NOT NULL,
   `nome` varchar(50) NOT NULL,
+  `cpf` varchar(11) NOT NULL,
   `email` varchar(50) DEFAULT NULL,
   `senha` varchar(255) NOT NULL,
   `nivel` tinyint(4) NOT NULL,
   `status` varchar(7) NOT NULL,
-  `datadecadastro` datetime NOT NULL
+  `datadecadastro` datetime NOT NULL,
+  `criado_por_cpf` varchar(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `users`
+--
+
+INSERT INTO `users` (`id`, `matricula`, `nome`, `cpf`, `email`, `senha`, `nivel`, `status`, `datadecadastro`, `criado_por_cpf`) VALUES
+(4, 1234, 'Usuario Supervisor', '12345678901', 'teste@gmail.com', '$2y$10$Y.hMIJAMgsWl/Ip1hzxqF.Sf.IeP0lcHCpoeQiJbPcDuDjrCfua0u', 2, 'Ativo', '2025-08-12 22:08:19', ''),
+(9, 123, 'Usuario Operador', '00000000000', 'teste@gmail.com', '$2y$10$Y.hMIJAMgsWl/Ip1hzxqF.Sf.IeP0lcHCpoeQiJbPcDuDjrCfua0u', 1, 'Ativo', '2025-08-12 22:08:19', ''),
+(10, 12345, 'Usuario Gerente', '11122233344', 'teste@gmail.com', '$2y$10$Y.hMIJAMgsWl/Ip1hzxqF.Sf.IeP0lcHCpoeQiJbPcDuDjrCfua0u', 3, 'Ativo', '2025-08-12 22:08:19', '');
 
 --
 -- Índices para tabelas despejadas
@@ -298,7 +351,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT de tabela `acessosniveis`
 --
 ALTER TABLE `acessosniveis`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de tabela `almoxarifados`
@@ -334,19 +387,19 @@ ALTER TABLE `itens`
 -- AUTO_INCREMENT de tabela `servicos_os`
 --
 ALTER TABLE `servicos_os`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT de tabela `servicos_tipos`
 --
 ALTER TABLE `servicos_tipos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de tabela `servico_etapas`
 --
 ALTER TABLE `servico_etapas`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de tabela `servico_etapas_observacao`
@@ -370,7 +423,7 @@ ALTER TABLE `unidades`
 -- AUTO_INCREMENT de tabela `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
